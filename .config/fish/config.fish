c0e56a18922f26b43d1de6e86c6f75880e975e18
Jesse Bu <jesse.bu@partners-cap.com>
2023-01-20 20:19:24 +0000

    update fish config
    update fish config
    
    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,10 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -gx EDITOR hx
+
+    fish_add_path /home/vagrant/python_envs/global/bin 
+    fish_add_path /home/vagrant/go/bin 
+    fish_add_path /usr/local/go/bin 
+    fish_add_path /home/vagrant/.fzf/bin 
+    fish_add_path /home/vagrant/.cargo/bin
 end
