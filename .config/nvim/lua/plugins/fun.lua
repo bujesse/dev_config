3a9579ab377eb0906ffbba4ff7fae505a44726e7
Jesse Bu <bu.jesse@gmail.com>
2025-02-07 08:26:16 -0500

    feat(plugin): add typr

--- .config/nvim/lua/plugins/fun.lua
+++ .config/nvim/lua/plugins/fun.lua
@@ -6,4 +6,17 @@ return {
       vim.api.nvim_create_user_command('FML', 'CellularAutomaton make_it_rain', {})
     end,
   },
+  {
+    'nvzone/typr',
+    dependencies = 'nvzone/volt',
+    opts = {},
+    keys = {
+      {
+        '<leader>tt',
+        '<cmd>Typr<CR>i',
+        { desc = 'Typing Test', noremap = true, silent = true },
+      },
+    },
+    cmd = { 'Typr', 'TyprStats' },
+  },
 }
